d0a1b44e433005a7e7a98c7a6abffb2f346eb6e9
Daniel <anwoo1214@gmail.com>
2018-08-29 08:50:11 -0700

    Added countdown timer on switch to turn off after amount of time

--- app/src/main/java/com/example/danieluselessmachine/MainActivity.java
+++ app/src/main/java/com/example/danieluselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.danieluselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "OvO b", Toast.LENGTH_SHORT).show();
                 }
                 else{
@@ -33,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked()){
+                    Log.d(TAG, "onTick: >_<");
+                    cancel();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinish: ;-;");
+            }
+        }.start();
+    }
+
 
     private void wireWidgets() {
         buttonSelfDestruct = findViewById(R.id.button_main_tm64);
